2f3bfbb80ad32221eac679fdc60c5b33be3d2fae
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:05:18 -0300

    Update FuncionarioController.java

--- src/main/java/br/com/senac/agendamentoapi/controller/FuncionarioController.java
+++ src/main/java/br/com/senac/agendamentoapi/controller/FuncionarioController.java
@@ -9,12 +9,14 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/funcionarios")
 @Tag(name = "Funcionários")
+@CrossOrigin("*")    
 public class FuncionarioController {
 
     private final FuncionarioService service;
